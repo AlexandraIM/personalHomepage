da3a6d78a5ad6360813438766a0f97d4b600b02f
Alexandra Imereli <alexandra.imereli@gmail.com>
2017-06-16 11:31:51 +0300

    implement responsive menu

--- mypage/pages/header.jsx
+++ mypage/pages/header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router'
 import { prefixLink } from 'gatsby-helpers'
+import FontAwesome from 'react-fontawesome'
 import me from './img/me.jpeg'
 
 
@@ -8,20 +9,45 @@ import me from './img/me.jpeg'
   render() {
     return (
       <div className="navigation" >
-        <ul>
-            <li>
-                <Link to={prefixLink('/')}>About Me</Link>
-            </li>
-            <li>
-              <Link to={prefixLink('/experiance/')}>Experiance</Link>
-            </li>
-            <li>
-                <Link to={prefixLink('/contacts/')}>Contacts</Link>
-            </li>
-        </ul>
+        <div className="wideDiv">
+          <ul>
+              <li>
+                  <Link to={prefixLink('/')}>About Me</Link>
+              </li>
+              <li>
+                <Link to={prefixLink('/experiance/')}>Experiance</Link>
+              </li>
+              <li>
+                  <Link to={prefixLink('/contacts/')}>Contacts</Link>
+              </li>
+          </ul>
+        </div>
+        <div className="navNarrow">
+            <FontAwesome name='bars' size='2x' onClick={this.toggle}/>
+              <ul className="links">
+                  <li>
+                      <Link to={prefixLink('/')} onClick={this.toggle}>About Me</Link>
+                  </li>
+                  <li>
+                    <Link to={prefixLink('/experiance/')}  onClick={this.toggle}>Experiance</Link>
+                  </li>
+                  <li>
+                      <Link to={prefixLink('/contacts/')}  onClick={this.toggle}>Contacts</Link>
+                  </li>
+              </ul>
+        </div>
       </div>
     )
+    
   }
+  toggle(){
+      let links = document.querySelector('.links');
+        if (links.style.display === 'block') {
+            links.style.display = 'none';
+        } else {
+            links.style.display = 'block';
+        }
+    }
 }
 export default class Header extends React.Component{
 
@@ -37,3 +63,5 @@ export default class Header extends React.Component{
     )
   }
 }
+
+
